89ae1a2b2154eb7f8b5f4b3710fbad4ab4ce64b8
dragon <1573511441@qq.com>
2023-07-09 16:02:09 +0800

    feat: init wiki

---
+++ src/main/java/com/adminwiki/javawikione/JavaWikiOneApplication.java
@@ -0,0 +1,13 @@
+package com.adminwiki.javawikione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWikiOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWikiOneApplication.class, args);
+    }
+
+}
