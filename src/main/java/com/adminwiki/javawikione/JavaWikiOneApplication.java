2692f790b02e4cd3cc45bab4033ed93ce469c619
dragon <1573511441@qq.com>
2023-07-09 16:57:26 +0800

    feat: add log, port, tip start success

--- src/main/java/com/adminwiki/javawikione/JavaWikiOneApplication.java
+++ src/main/java/com/adminwiki/javawikione/JavaWikiOneApplication.java
@@ -1,13 +1,23 @@
 package com.adminwiki.javawikione;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class JavaWikiOneApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(JavaWikiOneApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(JavaWikiOneApplication.class, args);
+//        SpringApplication.run(JavaWikiOneApplication.class, args);
+
+        SpringApplication app = new SpringApplication(JavaWikiOneApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("start success！！");
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
